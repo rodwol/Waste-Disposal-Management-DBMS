a61e104bd049c32fb516bad22c4129e4028c49dc
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2024-03-28 20:24:49 +0200

    Models updated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+#Models
 class Truck:
     def __init__(self, license_plate, type):
         self.license_plate = license_plate
@@ -6,15 +8,28 @@ class Truck:
         self.driver = None
         self.route = None
 
+    def assign_driver(self, driver):
+        self.driver = driver
+
+    def assign_route(self, route):
+        self.route = route
+
 class Driver:
     def __init__(self, name, phone_number):
         self.name = name
         self.phone_number = phone_number
         self.truck = None
 
+    def assign_truck(self, truck):
+        self.truck = truck
+
 class Route:
     def __init__(self, area):
         self.area = area
+        self.truck = None
+
+    def assign_truck(self, truck):
+        self.truck = truck
 
 class WasteManagementSystem:
     def __init__(self):
